2e4b1fd0a44407afd9a6af562f8910bafa8bcf1e
毛妍慧 <maoyanhui@maomao.local>
2018-04-24 15:04:39 +0800

    init

--- main/main.js
+++ main/main.js
@@ -1,6 +1,54 @@
 // Write your cade below:
-function main() {};
+function ifNumber(output) {
+    var count = 0;
+    output.forEach(item=>{
+        if(typeof (item) == 'number'){
+            count++;
+        }
+    });
+    return count;
+}
+function inObject(collection) {
+    var information = [];
+    collection.forEach((item,index)=>{
+        information.push({"item":item,"index":index});
+    });
+    return information;
+}
+function judge(input,output) {
+    var elementNumber = 0;
+    var positionNumber = 0;
+    input.forEach(inputItem=>{
+        output.forEach(outputItem=>{
+            if(inputItem.item == outputItem.item&&inputItem.index == outputItem.index){
+                positionNumber++;
+            }
+            else if(inputItem.item == outputItem.item){
+                elementNumber++;
+            }
+        });
+    });
+    return positionNumber+'A'+elementNumber+'B';
+}
+function guessNumber(input,output) {
+    if(output.length == 4){
+        if(ifNumber(output) == 4){
+            var inputInformation = inObject(input);
+            var outputInformation = inObject(output);
+            var result = judge(inputInformation,outputInformation);
+        }
+        else {
+            result = "请确认输入的元素为数字！";
+        }
+    }
+    else {
+        result = '请输入4个数字！';
+    }
+    return result;
+}
 
-module.exports = {
-    main
+module.exports = function main(intput,output) {
+    var result = guessNumber(intput,output);
+    console.log(result);
+    return result;
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,54 @@
 const main = require('../main/main');
-
+var guessNumber = require('../main/main.js');
 describe('main()', () => {
 
+    var input;
+    var output1,output2,output3,output4,output5,output6,output7
+
+    beforeEach(()=>{
+        input = [1,2,3,4];
+        output1 = [5,1,3,4];
+        output2 = [1,2,3,4];
+        output3 = [5,6,7,8];
+        output4 = [4,3,2,1];
+        output5 = [6,3,2,1];
+        output6 = [3,4,5,6,7];
+        output7 = [1,2,3,'a'];
+    });
+
+    it("first guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output1);
+        expect(console.log).toHaveBeenCalledWith("2A1B");
+    });
+    it("second guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output2);
+        expect(console.log).toHaveBeenCalledWith("4A0B");
+    });
+    it("third guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output3);
+        expect(console.log).toHaveBeenCalledWith("0A0B");
+    });
+    it("fourth guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output4);
+        expect(console.log).toHaveBeenCalledWith("0A4B");
+    });
+    it("fifth guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output5);
+        expect(console.log).toHaveBeenCalledWith("0A3B");
+    });
+    it("sixth guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output6);
+        expect(console.log).toHaveBeenCalledWith("请输入4个数字！");
+    });
+    it("seventh guess number",() => {
+        spyOn(console,"log");
+        guessNumber(input,output7);
+        expect(console.log).toHaveBeenCalledWith("请确认输入的元素为数字！");
+    });
 });
